343f05fdc8f913a6144b920d5e55293d3ca1ec85
Krisna <krisna.post@gmail.com>
2025-07-17 20:13:04 +0700

    Fix: Change pgdata path

--- logs/manifests/run.sh
+++ logs/manifests/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sops --decrypt secret.enc.yaml | kubectl apply -f - 
-sleep 5
+sleep 1
 kubectl apply -f configmap.yaml
-sleep 5
+sleep 1
 kubectl apply -f statefulset.yaml
-sleep 5
+sleep 3
 kubectl apply -f deployment.yaml
-sleep 5
+sleep 3
 kubectl apply -f combined-service.yaml
-sleep 5
+sleep 1
 kubectl apply -f ingress.yaml
