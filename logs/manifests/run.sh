71df78e6e612c5f07c9d764f3713c8c9afa59316
Krisna <krisna.post@gmail.com>
2025-07-22 15:50:12 +0700

    feat: deploy to cloud, change build image docker to multi platform

--- logs/manifests/run.sh
+++ logs/manifests/run.sh
@@ -4,9 +4,9 @@ sleep 1
 kubectl apply -f configmap.yaml
 sleep 1
 kubectl apply -f statefulset.yaml
-sleep 3
+sleep 5
 kubectl apply -f deployment.yaml
-sleep 3
+sleep 5
 kubectl apply -f combined-service.yaml
-sleep 1
-kubectl apply -f ingress.yaml
+# sleep 30
+# kubectl apply -f ingress.yaml
