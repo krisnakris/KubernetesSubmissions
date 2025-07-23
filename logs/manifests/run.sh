9bd78f95a18aadbe84930bf77b0d6b859235ffe8
Krisna <krisna.post@gmail.com>
2025-07-23 17:18:01 +0700

    feat: create api gateway

--- logs/manifests/run.sh
+++ logs/manifests/run.sh
@@ -6,9 +6,13 @@ sleep 1
 kubectl apply -f statefulset.yaml
 sleep 5
 kubectl apply -f deployment.yaml
-sleep 5
-kubectl apply -f combined-service.yaml
-sleep 5
-kubectl apply -f service.yaml
 sleep 1
-kubectl apply -f ingress.yaml
+kubectl apply -f combined-service.yaml
+# sleep 5
+# kubectl apply -f service.yaml
+# sleep 1
+# kubectl apply -f ingress.yaml
+sleep 1
+kubectl apply -f gateway.yaml
+sleep 1
+kubectl apply -f route.yaml
