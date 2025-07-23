c71778197f88caab854dedb01002898c65d9a59b
Krisna <krisna.post@gmail.com>
2025-07-23 19:28:59 +0700

    feat: rewrite url from /pingpong to / for pingpong backend

--- the_project/pingpong/routes/index.js
+++ the_project/pingpong/routes/index.js
@@ -2,10 +2,7 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ status: "oke", message: "Server is good" });
-});
-router.get("/pingpong", indexController.getPingpongCount);
+router.get("/", indexController.getPingpongCount);
 router.get("/pings", indexController.currentPingpongCounter);
 
 module.exports = router;
